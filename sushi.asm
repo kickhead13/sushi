62467dd0637c0c026fe2cbed96f181b6ea7283de
kickhead13 <ana.alexandru.gabriel@proton.me>
2026-02-22 19:06:24 +0200

    HUGE refactor MAX win: - piping - proper PATH support - fuck.

--- sushi.asm
+++ sushi.asm
@@ -1,379 +1,312 @@
 FORMAT elf64 EXECUTABLE 3
 SEGMENT READABLE EXECUTABLE WRITABLE
-deb_mess db "FAIL",10
-ls_command db "/usr/bin/",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-command_buffer rb 256
-buffer rb 200
-pipefd   rb 8
-pipefd1  rb 8
 
-MAX_PIPES=10
-MAX_ARGS=2000*8
-MAX_PIPE_ARGS=MAX_ARGS*MAX_PIPES
+deb_mess db "FAIL",10
+command_buffer rb 256
+full_path_buf rb 512
+buffer rb 200
+pipefd rb 8
+envp_ptr dq 0
+
+MAX_ARGS=1000*8
 BUFFER_SIZE=200
-ST_SIZE_OFFSET=48
-BIN_OFFSET=9
 
 entry main
 main:
-        mov r15, buffer
+        mov rax, [rsp]
+        lea rsi, [rsp + 16 + rax*8]
+        mov [envp_ptr], rsi
+
+main_loop:
         call clean_buffer
-        mov rax, 0                    ; READ SYSCALL on STDIN TO BUFFER
+        mov rax, 0
         mov rdi, 0
         mov rsi, buffer
         mov rdx, BUFFER_SIZE
         syscall
-
-        cmp rax, 0                   ; nothing read -> exit
-        je exit
-
-        call handle_cd_cmd
-
-        mov rax, 57                  ; fork
-        syscall
-
         cmp rax, 0
-        jl fail
+        jle exit_proc
 
+        call handle_cd
+        mov rax, 57
+        syscall
         cmp rax, 0
-        jne wait_and_main
+        jne wait_parent
+        jmp execute_chain
 
-start_copy:
-        mov rcx, 0
-        mov rax, 9
-        mov rdi, 0
-        mov rsi, MAX_ARGS
-        mov rdx, 3
-        mov r10, 34
-        mov r8, 0
-        mov r9, 0
+wait_parent:
+        mov rax, 61
+        mov rdi, -1
+        xor rsi, rsi
+        xor rdx, rdx
+        xor r10, r10
         syscall
+        jmp main_loop
 
-        cmp rax, 0xffffffffffffffff
-        je fail
-
-        mov rdi, command_buffer
-        mov rsi, ls_command
-
-        push rax
-        push rax
-        pop rdx
-        pop r12
-
-copy_first:
-        mov al, [rsi]
-        mov [rdi], al
-        inc rsi
-        inc rdi
-        test al, al
-        jnz copy_first
-
-        dec rdi  
-
-        mov rsi, r15
-        mov r15, 0
-
-        mov QWORD [rdx], rdi
-        add rdx, 8
-copy_second:
-        mov al, [rsi]
-        cmp al, 10
-        je skip_newline
-
-        cmp al, '>'
-        je redirection
-
-        cmp al, 34
-        je double_quote
-
-        cmp al, '|'
-        je register_for_pipe
-
-        cmp al, 32
-        je split
-dont_skip_char:        
-        mov [rdi], al
-skip_char:
-        inc rdi
-skip_newline:
-        inc rsi
-        test al, al
-        jnz copy_second
-        add rdx, 8
-exit_copy:
-        mov QWORD [rdx], 0
-
-        mov rax, 59
-        mov rdi, command_buffer
-        mov rsi, r12
-        mov rdx, 0
-        syscall
-        
-        cmp rax, -1
-        je fail
-
+exit_proc:
         mov rax, 60
-        mov rdi, 0
+        xor rdi, rdi
         syscall
-exit:
-        mov rax, 60
-        mov rdi, 0
-        syscall
+
 fail:
         mov rax, 1
         mov rdi, 2
         mov rsi, deb_mess
-        mov rdx, 4
+        mov rdx, 5
         syscall
-        
         mov rax, 60
         mov rdi, 1
         syscall
-wait_and_main:
-        cmp rcx, 0
-        jne main
 
-        mov rax, 61
-        mov rdi, 0
-        mov rsi, 0
-        mov rdx, 0
-        mov r10, 0
-        syscall
-        
-        jmp main
-split:
-        cmp r15, 0
-        jne dont_skip_char
-        
-        mov byte [rdi], 0
-
-        inc rdi
-        mov QWORD [rdx], rdi
-        add rdx, 8
-        dec rdi
-        
-        jmp skip_char
-
-inc_rdi_and_skip_char:
-        inc rdi
-        jmp skip_char
-
-handle_cd_cmd:
-        ; check command starts with 'cd'
-
-        mov r12, buffer
-        cmp byte [r12], 'c'
-        jne .rtrn
-
-        inc r12
-        cmp byte [r12], 'd'
-        jne .rtrn
-
-        inc r12
-        inc r12
-        
-        mov r11, r12
-        
-.to_end_path:
-        inc r11
-        cmp byte [r11], 10
-        jne .to_end_path
-        mov byte [r11], 0
-
-        ; call chdir to path
-        mov rax, 80
-        mov rdi, r12
-        syscall
-
-        cmp rax, -1
-        je fail
-        jmp main
-.rtrn:
-        ret
-        
-clean_buffer:
-        mov r12, BUFFER_SIZE
-        mov r11, buffer
-.cloop:
-        mov byte [r11], 0
-        inc r11
-        sub r12, 1
-
-        cmp r12, 0
-        jne .cloop
-
-        ret
-
-clean_command_buffer:
-        mov r12, 256
-        mov r11, command_buffer
-.cloop:
-        mov byte [r11], 0
-        inc r11
-        sub r12, 1
-
-        cmp r12, 0
-        jne .cloop
-
-        ret
-double_quote:
-        not r15
-        inc rsi
-        jmp copy_second
-
-redirection:
-        cmp r15, 0
-        jne dont_skip_char
-
-        push rdi
-        push rdx
-        push r11
-        push rsi
-
+handle_cd:
+        mov rsi, buffer
+        cmp byte [rsi], 'c'
+        jne .out
+        cmp byte [rsi+1], 'd'
+        jne .out
+        add rsi, 3
         mov rdi, rsi
+.fnl:
+        cmp byte [rdi], 10
+        je .t
+        cmp byte [rdi], 0
+        je .t
         inc rdi
-        inc rdi
-
-        mov r11, rdi
-
-.loop_to_newline:
-        inc r11
-        cmp BYTE [r11], 10
-        jne .loop_to_newline
-
-        mov BYTE [r11], 0
-
-        mov rax, 2
-        mov rsi, 578
-        mov rdx, 448
+        jmp .fnl
+.t:
+        mov byte [rdi], 0
+        mov rax, 80
+        mov rdi, rsi
         syscall
+        pop rax
+        jmp main_loop
+.out:
+        ret
 
+clean_buffer:
+        mov rcx, BUFFER_SIZE
+        mov rdi, buffer
+        xor al, al
+        rep stosb
+        ret
 
-        cmp rax, -1
-        je fail
+execute_chain:
+        mov r12, buffer
+.chain_loop:
+        mov rsi, r12
+.find_pipe:
+        mov al, [rsi]
+        cmp al, 10
+        je .last_cmd
+        cmp al, 0
+        je .last_cmd
+        cmp al, '|'
+        je .found_pipe
+        inc rsi
+        jmp .find_pipe
 
-        ; dup2 fd -> stdout
-        mov rdi, rax
-        mov rax, 33
-        mov rsi, 1
-        syscall
+.found_pipe:
+        mov byte [rsi], 0
+        lea r13, [rsi + 1]
+.skip_s:
+        cmp byte [r13], 32
+        jne .p_go
+        inc r13
+        jmp .skip_s
 
-        cmp rax, -1
-        je fail
-
-        pop rsi
-        pop r11
-        pop rdx
-        pop rdi
-
-        sub rdx, 8
-        jmp exit_copy
-
-register_for_pipe:
-
-        
-        push rsi
-        add rdx, 8
-
-        mov QWORD [rdx], 0
-
-        mov rdi, 0
-        mov edi, DWORD [pipefd]
-
-        push rdi
-        ; pipe
+.p_go:
         mov rax, 22
         mov rdi, pipefd
         syscall
 
-        pop rdi
-        ; fork
         mov rax, 57
         syscall
-
         cmp rax, 0
-        jg left_hand_side
+        jne .p_parent
 
-        cmp rax, 0
-        jne fail
-
-        cmp rcx, 0
-        jne dup2_stdin
-dup2_stdin_ret:
-        ; dup2 pipefd[1] -> stdout
         mov rax, 33
-        mov edi, DWORD [pipefd+4]
+        mov edi, dword [pipefd+4]
         mov rsi, 1
         syscall
+        mov rax, 3
+        mov edi, dword [pipefd]
+        syscall
+        mov rax, 3
+        mov edi, dword [pipefd+4]
+        syscall
+        mov rsi, r12
+        jmp do_exec
 
-        cmp rax, -1
+.p_parent:
+        mov rax, 33
+        mov edi, dword [pipefd]
+        mov rsi, 0
+        syscall
+        mov rax, 3
+        mov edi, dword [pipefd]
+        syscall
+        mov rax, 3
+        mov edi, dword [pipefd+4]
+        syscall
+        mov r12, r13
+        jmp .chain_loop
+
+.last_cmd:
+        mov byte [rsi], 0
+        mov rsi, r12
+        jmp do_exec
+
+do_exec:
+        mov r14, rsi
+        mov rax, 9
+        xor rdi, rdi
+        mov rsi, MAX_ARGS
+        mov rdx, 3
+        mov r10, 34
+        xor r8, r8
+        xor r9, r9
+        syscall
+        mov rbx, rax
+        mov rdx, rbx
+        mov rsi, r14
+.skip_initial_spaces:
+        cmp byte [rsi], 32
+        jne .start_parse
+        inc rsi
+        jmp .skip_initial_spaces
+.start_parse:
+        mov rdi, command_buffer
+        mov [rdx], rdi
+        add rdx, 8
+
+.parse:
+        mov al, [rsi]
+        cmp al, 0
+        je .done
+        cmp al, 32
+        je .s
+        cmp al, 10
+        je .done
+        cmp al, '>'
+        je .redir
+        mov [rdi], al
+        inc rdi
+        inc rsi
+        jmp .parse
+.s:
+        mov byte [rdi], 0
+        inc rdi
+        inc rsi
+.ss:
+        cmp byte [rsi], 32
+        jne .sn
+        inc rsi
+        jmp .ss
+.sn:
+        cmp byte [rsi], 10
+        je .done
+        cmp byte [rsi], 0
+        je .done
+        mov [rdx], rdi
+        add rdx, 8
+        jmp .parse
+.redir:
+        mov byte [rdi], 0
+        inc rsi
+.rs:
+        cmp byte [rsi], 32
+        jne .rf
+        inc rsi
+        jmp .rs
+.rf:
+        mov rdi, rsi
+.rl:
+        cmp byte [rsi], 32
+        je .re
+        cmp byte [rsi], 10
+        je .re
+        cmp byte [rsi], 0
+        je .re
+        inc rsi
+        jmp .rl
+.re:
+        mov byte [rsi], 0
+        mov rax, 2
+        mov rsi, 578
+        mov rdx, 448
+        syscall
+        mov rdi, rax
+        mov rax, 33
+        mov rsi, 1
+        syscall
+        jmp .done
+
+.done:
+        mov byte [rdi], 0
+        mov qword [rdx], 0
+        
+        mov rsi, command_buffer
+        cmp byte [rsi], '/'
+        je .direct
+        cmp byte [rsi], '.'
+        je .direct
+
+        mov r8, [envp_ptr]
+.f_path:
+        mov rsi, [r8]
+        test rsi, rsi
+        jz .direct
+        cmp dword [rsi], "PATH"
+        jne .n_env
+        cmp byte [rsi+4], "="
+        je .p_found
+.n_env:
+        add r8, 8
+        jmp .f_path
+
+.p_found:
+        add rsi, 5
+        mov r9, rsi
+.p_l:
+        mov rdi, full_path_buf
+.c_d:
+        mov al, [r9]
+        cmp al, ':'
+        je .d_d
+        cmp al, 0
+        je .d_d
+        mov [rdi], al
+        inc rdi
+        inc r9
+        jmp .c_d
+.d_d:
+        mov byte [rdi], '/'
+        inc rdi
+        mov rsi, command_buffer
+.c_c:
+        mov al, [rsi]
+        test al, al
+        jz .att
+        mov [rdi], al
+        inc rdi
+        inc rsi
+        jmp .c_c
+.att:
+        mov byte [rdi], 0
+        mov rax, 59
+        mov rdi, full_path_buf
+        mov rsi, rbx
+        xor rdx, rdx
+        syscall
+        cmp byte [r9], 0
         je fail
+        inc r9
+        jmp .p_l
 
-        ; execve
+.direct:
         mov rax, 59
         mov rdi, command_buffer
-        mov rsi, r12
-        mov rdx, 0
+        mov rsi, rbx
+        xor rdx, rdx
         syscall
-
-        cmp rax, -1
-        je fail
-
-        mov rax, 60
-        mov rdi, 0
-        syscall
-
-        
-left_hand_side:
-        mov rcx, 1
-        ; dup2 pipefd[0] -> stdin
-        mov rax, 33
-        mov edi, DWORD [pipefd]
-        mov rsi, 0
-        syscall
-
-        call clean_command_buffer
-        pop rsi
-        inc rsi
-        inc rsi
-        ; inc rsi
-
-        mov r15, rsi
-        
-        jmp start_copy
-
-dup2_stdin:
-        mov rax, 33
-        mov rsi, 0
-        syscall
-        
-        cmp rax, -1
-        je fail
-
-
-        jmp dup2_stdin_ret 
-
-debug:
-        push rax
-        push rdi
-        push rsi
-        push rdx
-        
-        ; mov rax, 1
-        ; mov rdi, 1
-        ; mov rsi, r15
-        ; mov rdx, 10
-        ; syscall
-
-        mov rax, 1
-        mov rdi, 2
-        mov rsi, command_buffer
-        mov rdx, 3
-        syscall
-
-        pop rdx
-        pop rsi
-        pop rdi
-        pop rax
-
-        ret
+        jmp fail
